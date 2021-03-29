36d73aceac7d217087c5df9c9fb79bffe1cd4dee
akrindev <akrinmin@gmail.com>
2021-03-29 22:01:39 +0700

    simple route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    hellow iam home
+@endsection
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,5 @@
+<div class="p-5 flex flex-col">
+    <a href="{{ route('home') }}"> home </a>
+    <a href="{{ route('setting') }}"> setting </a>
+    <a href="{{ route('post') }}"> post </a>
+</div>
